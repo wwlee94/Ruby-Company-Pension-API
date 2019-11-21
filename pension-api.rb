a36da8f4f77c21e008a7aa705ceb03c2534dae29
wwlee94 <wwlee94@naver.com>
2019-11-21 10:38:51 +0900

    PensionApi 형식을  module -> class 로 변경 후 코드 리팩토링

--- pension-api.rb
+++ pension-api.rb
@@ -3,155 +3,122 @@ require 'rest-client'
 require 'nokogiri'
 require 'json'
 
-module PensionAPI
-  def PensionAPI::set_api_key()
-    api_key = "7UmYLGjQHqLWPassQCa8xuuFiQ9ZWKGKMiQydPO7cBvLTDFL6ZGpdZZDMhxb9ZJw8CNzhf06dJSsjmply1XZMA=="
-    return api_key
-  end
+class PensionApi
+  BASE_API_URL = 'http://apis.data.go.kr/B552015/NpsBplcInfoInqireService'.freeze
+  API_KEY = '7UmYLGjQHqLWPassQCa8xuuFiQ9ZWKGKMiQydPO7cBvLTDFL6ZGpdZZDMhxb9ZJw8CNzhf06dJSsjmply1XZMA=='.freeze
+  MAXIMUM_HISTORY_INDEX = 100
 
-  def PensionAPI::get_history_index(company_name, registration_number, recent = true)
-    url = 'http://apis.data.go.kr/B552015/NpsBplcInfoInqireService/getBassInfoSearch'
-    api_key = set_api_key()
-    headers = {
+  class << self
+    def get_history_index(company_name, registration_number, recent = true)
+      url = "#{BASE_API_URL}/getBassInfoSearch"
+      headers = {
         params: {
-            'ServiceKey' => api_key,
-            'wkpl_nm' => company_name,
-            'bzowr_rgst_no' => parse_registration_number(registration_number),
-            'pageNo' => '1',
-            'numOfRows' => '100'
+          ServiceKey: API_KEY,
+          wkpl_nm: company_name,
+          bzowr_rgst_no: parse_registration_number(registration_number),
+          pageNo: '1',
+          numOfRows: MAXIMUM_HISTORY_INDEX.to_s
         }
-    }
-
-    begin
-      response = RestClient.get(url, headers)
-    rescue StandardError
-      puts("PensionAPI Error : 회사 식별변호를 가져올 수 없습니다.")
-    end
-
-    seq_list = []
-    response = Nokogiri::XML(response)
-
-    info = {
-      'items' => []
-    }
-    # company = response.search('item').xpath('wkplNm/text()')[0]
-    # puts(company)
-    response.search('item').each do |item|
-      company = item.at('wkplNm').text
-      date = item.at('dataCrtYm').text
-      seq = item.at('seq').text
-
-      info['items'] << {
-        company: company,
-        date: date,
-        seq: seq
       }
-    end
 
-    info["items"] = info["items"].sort_by{|index| index[:date]}.reverse
+      begin
+        response = RestClient.get(url, headers)
+      rescue StandardError
+        puts('PensionAPI Error : 회사 식별변호를 가져올 수 없습니다.')
+        return
+      end
 
-    if recent
-      current = info["items"][0]
+      response = Nokogiri::XML(response)
+
       info = {
-        'items' => []
+        items: []
       }
-      info['items'] << current
-      return info
-    else
-      return info
-    end
-  end
+      response.search('item').each do |item|
+        company = item.at('wkplNm').text
+        date = item.at('dataCrtYm').text
+        seq = item.at('seq').text
 
-  def PensionAPI::get_detailed_info(index)
-    # url 가져온후
-    url = "http://apis.data.go.kr/B552015/NpsBplcInfoInqireService/getDetailInfoSearch"
-    api_key = set_api_key()
-    headers = {
-        params: {
-            'ServiceKey' => api_key,
-            'seq' => index[:seq]
+        info[:items] << {
+          company: company,
+          date: date,
+          seq: seq
         }
-    }
+      end
 
-    begin
-      response = RestClient.get(url, headers)
-    rescue StandardError
-      puts("PensionAPI Error : 회사 세부 정보를 가져올 수 없습니다.")
+      info[:items] = info[:items].sort_by { |index| index[:date] }.reverse
+
+      if recent
+        current = info[:items].first
+
+        info = {
+          items: []
+        }
+        info[:items] << current
+      end
+
+      info
     end
 
-    items = Nokogiri::XML(response)
-
-    items.search('item').each do |item|
-      employee_count = item.at('jnngpCnt').text.to_i
-      paid_pension = item.at('crrmmNtcAmt').text.to_i
-      pension_per_employee = (paid_pension/employee_count/ 2).to_i
-
-    info = {
-      company: index[:company],
-      date: index[:date],
-      # seq: index[:seq],
-      employee_count: employee_count,
-      paid_pension: paid_pension,
-      pension_per_employee: pension_per_employee
-    }
-
-    return info
-    end
-
-  end
-
-  # (회사 이름, 데이터 생성일, 직원수, 회사 총 연금, 직원 1인 평균 연금)
-  def PensionAPI::get_company_info(company_name, registration_number, recent = true)
-    index = get_history_index(company_name, registration_number, recent)
-    # if index.include?('exception_msg')
-    #   return {
-    #     'statusCode' => 400,
-    #     'exception_msg' => index[:exception_msg],
-    #     'message' => '정보를 조회할 수 없습니다.'
-    #   }
-    # end
-
-    result = []
-    for info in index['items'] do
-      detail_info = get_detailed_info(info)
-      # if detail_info.include?('exception_msg')
-      #   return {
-      #     'statusCode' => 400,
-      #     'exception_msg' => detail_info[:exception_msg],
-      #     'message' => '정보를 조회할 수 없습니다.'
-      #   }
-      # end
-      result.push(detail_info)
-    end
-
-    return {
-      'statusCode' => 200,
-      'body' => result.to_json
-    }
-  end
-
-  # 최근 10개 정보 데이터 (생성일, 직원 수)
-  def PensionAPI::get_company_count(company_name, registration_number, recent = true)
-    company_info = get_company_info(company_name, registration_number, recent)
-    json_info = JSON.parse(company_info["body"])
-    list = []
-
-    for info in json_info do
-      date = info["date"]
-      count = info["employee_count"]
-      tmp = {
-        date: date,
-        count: count
+    def get_detailed_info(index)
+      url = "#{BASE_API_URL}/getDetailInfoSearch"
+      headers = {
+        params: {
+          ServiceKey: API_KEY,
+          seq: index[:seq]
+        }
       }
-      list.push(tmp)
-    end
-    return {
-      'statusCode' => 200,
-      'body' => list.to_json
-    }
-  end
-end
 
-def parse_registration_number(number)
-  return number.scan(/(\d{3})-(\d{2})-(\d{1})/).join("")
+      begin
+        response = RestClient.get(url, headers)
+      rescue StandardError
+        puts('PensionAPI Error : 회사 세부 정보를 가져올 수 없습니다.')
+        return
+      end
+
+      item = Nokogiri::XML(response).search('item')[0]
+
+      {
+        company: index[:company],
+        date: index[:date],
+        employee_count: item.at('jnngpCnt').text.to_i,
+        paid_pension: item.at('crrmmNtcAmt').text.to_i
+      }
+    end
+
+    # (회사 이름, 데이터 생성일, 직원수, 회사 총 연금, 직원 1인 평균 연금)
+    def get_company_info(company_name, registration_number, recent = true)
+      index = get_history_index(company_name, registration_number, recent)
+
+      result = []
+      index[:items].each do |info|
+        detail_info = get_detailed_info(info)
+        result.push(detail_info)
+      end
+
+      result
+    end
+
+    # 최근 100개 정보 중 직원 수 데이터 (생성일, 직원 수) 반환
+    def get_company_count(company_name, registration_number, recent = true)
+      company_info = get_company_info(company_name, registration_number, recent)
+      # json_info = JSON.parse(company_info["body"])
+      list = []
+
+      for info in company_info do
+        date = info[:date]
+        count = info[:employee_count]
+        tmp = {
+          date: date,
+          count: count
+        }
+        list.push(tmp)
+      end
+
+      list
+    end
+
+    def parse_registration_number(number)
+      number.scan(/(\d{3})-(\d{2})-(\d)/).join('')
+    end
+  end
 end
--- test_employ_count.rb
+++ test_employ_count.rb
@@ -2,5 +2,5 @@ require './pension-api'
 require 'json'
 
 # 최근 10개 정보의 직원 수 리스트
-count = PensionAPI::get_company_count('당근마켓', '375-87-00088', true)
+count = PensionApi.get_company_count('당근마켓', '375-87-0', false)
 puts(count)
